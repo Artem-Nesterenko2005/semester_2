3d1cb8eaf9df58ecb6f3ce8d4633eec712e6c092
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-02-28 00:01:35 +0500

    BWT
    add class Main, class BWT

--- Burrows-Wheeler/Program.cs
+++ Burrows-Wheeler/Program.cs
@@ -1,159 +1,168 @@
-﻿void sort(char[,] array)
+﻿class BWT
 {
-    for (int i = 0; i < array.GetLength(1) - 1; i++)
+    public static void sort(char[,] array)
     {
-        for (int j = i + 1; j < array.GetLength(1); ++j)
+        for (int i = 0; i < array.GetLength(1) - 1; i++)
         {
-            for (int m = 0; m < array.GetLength(1); ++m)
+            for (int j = i + 1; j < array.GetLength(1); ++j)
             {
-                if (array[i, m] == array[j, m])
+                for (int m = 0; m < array.GetLength(1); ++m)
                 {
-                    continue;
-                }
-
-                if (array[i, m] > array[j, m])
-                {
-                    for (int k = 0; k < array.GetLength(1); ++k)
+                    if (array[i, m] == array[j, m])
                     {
-                        char swap = array[i, k];
-                        array[i, k] = array[j, k];
-                        array[j, k] = swap;
+                        continue;
                     }
+
+                    if (array[i, m] > array[j, m])
+                    {
+                        for (int k = 0; k < array.GetLength(1); ++k)
+                        {
+                            char swap = array[i, k];
+                            array[i, k] = array[j, k];
+                            array[j, k] = swap;
+                        }
+                    }
+                    break;
                 }
+            }
+        }
+    }
+
+    public static string convertBack(string convertedString, int position, string sortArray)
+    {
+        string result = "";
+        for (int i = 0; i < convertedString.Length; ++i)
+        {
+            result += sortArray[position];
+
+            int numberCoincidences = 0;
+            for (int k = 0; k < position + 1; k++)
+            {
+                if (sortArray[k] == sortArray[position])
+                {
+                    ++numberCoincidences;
+                }
+            }
+
+            int newPosition = 0;
+            int j = 0;
+            while (newPosition != numberCoincidences)
+            {
+                if (convertedString[j] == result[i])
+                {
+                    ++newPosition;
+                }
+                ++j;
+            }
+            position = j - 1;
+        }
+        return result;
+    }
+
+    public static void fillArray(char[,] array, string expression)
+    {
+        for (int i = 0; i < expression.Length; ++i)
+        {
+            array[0, i] = expression[i];
+        }
+
+        for (int i = 1; i < expression.Length; ++i)
+        {
+            for (int j = 0; j < expression.Length; ++j)
+            {
+                array[i, j] = array[i - 1, (j + 1) % expression.Length];
+            }
+        }
+    }
+
+    public static int checkPosition(string expression, char[,] array)
+    {
+        int position = 0;
+        for (int i = 0; i < expression.Length; ++i)
+        {
+            if (position != 0)
+            {
                 break;
             }
-        }
-    }
-}
-
-string convertBack(string convertedString, int position, string sortArray)
-{
-    string result = "";
-    for (int i = 0; i < convertedString.Length; ++i)
-    {
-        result += sortArray[position];
-
-        int numberCoincidences = 0;
-        for (int k = 0; k < position + 1; k++)
-        {
-            if (sortArray[k] == sortArray[position])
+            for (int j = 0; j < expression.Length; ++j)
             {
-                ++numberCoincidences;
+                if (expression[j] != array[i, j])
+                {
+                    position = 0;
+                    break;
+                }
+                position = i;
             }
         }
+        return position;
+    }
 
-        int newPosition = 0;
-        int j = 0;
-        while (newPosition != numberCoincidences)
+    public static bool test()
+    {
+        string expression = "abacaba";
+        char[,] array = new char[expression.Length, expression.Length];
+        fillArray(array, expression);
+        sort(array);
+        string result = "";
+        for (int i = 0; i < expression.Length; ++i)
         {
-            if (convertedString[j] == result[i])
-            {
-                ++newPosition;
-            }
-            ++j;
+            result += array[i, expression.Length - 1];
         }
-        position = j - 1;
-    }
-    return result;
-}
-
-void fillArray(char[,] array, string expression)
-{
-    for (int i = 0; i < expression.Length; ++i)
-    {
-        array[0, i] = expression[i];
-    }
-
-    for (int i = 1; i < expression.Length; ++i)
-    {
-        for (int j = 0; j < expression.Length; ++j)
+        if (!string.Equals(result, "bcabaaa"))
         {
-            array[i, j] = array[i - 1, (j + 1) % expression.Length];
+            return false;
         }
-    }
-}
-
-int checkPosition(string expression, char[,] array)
-{
-    int position = 0;
-    for (int i = 0; i < expression.Length; ++i)
-    {
-        if (position != 0)
+        int position = checkPosition(expression, array);
+        if (position != 2)
         {
-            break;
+            return false;
         }
-        for (int j = 0; j < expression.Length; ++j)
+        string sortingString = "";
+        for (int i = 0; i < expression.Length; ++i)
         {
-            if (expression[j] != array[i, j])
-            {
-                position = 0;
-                break;
-            }
-            position = i;
+            sortingString += array[i, 0];
         }
+        return string.Equals(convertBack(result, position, sortingString), expression);
     }
-    return position;
 }
 
-bool test()
+class Program
 {
-    string expression = "abacaba";
-    char[,] array = new char[expression.Length, expression.Length];
-    fillArray(array, expression);
-    sort(array);
-    string result = "";
-    for (int i = 0; i < expression.Length; ++i)
+    public static void Main()
     {
-        result += array[i, expression.Length - 1];
+        if (!BWT.test())
+        {
+            Console.WriteLine("Error test");
+            return;
+        }
+        Console.Write("Enter the string ");
+        string expression = Console.ReadLine();
+        if (expression == null)
+        {
+            Console.WriteLine("Empty string");
+            return;
+        }
+
+        char[,] array = new char[expression.Length, expression.Length];
+        BWT.fillArray(array, expression);
+        BWT.sort(array);
+
+        string result = "";
+        for (int i = 0; i < expression.Length; ++i)
+        {
+            result += array[i, expression.Length - 1];
+        }
+        Console.WriteLine($"Converted string: {result}");
+
+        int position = BWT.checkPosition(expression, array);
+        Console.WriteLine($"Original string position: {position}");
+
+        string sortingString = "";
+        for (int i = 0; i < expression.Length; ++i)
+        {
+            sortingString += array[i, 0];
+        }
+        Console.Write($"Original string: {BWT.convertBack(result, position, sortingString)}");
+        return;
     }
-    if (!string.Equals(result,"bcabaaa"))
-    {
-        return false;
-    }
-    int position = checkPosition(expression, array);
-    if (position != 2)
-    {
-        return false;
-    }
-    string sortingString = "";
-    for (int i = 0; i < expression.Length; ++i)
-    {
-        sortingString += array[i, 0];
-    }
-    return string.Equals(convertBack(result, position, sortingString), expression);
 }
-
-if (!test())
-{
-    Console.WriteLine("Error test");
-    return;
-}
-Console.Write("Enter the string ");
-string expression = Console.ReadLine();
-if (expression == null)
-{
-    Console.WriteLine("Empty string");
-    return;
-}
-
-char[,] array = new char[expression.Length, expression.Length];
-fillArray(array, expression);
-sort(array);
-
-string result = "";
-for (int i = 0; i < expression.Length; ++i)
-{
-    result += array[i, expression.Length - 1];
-}
-Console.WriteLine($"Converted string: {result}");
-
-int position = checkPosition(expression, array);
-Console.WriteLine($"Original string position: {position}");
-
-string sortingString = "";
-for (int i = 0; i < expression.Length; ++i)
-{
-    sortingString += array[i, 0];
-}
-Console.Write($"Original string: {convertBack(result, position, sortingString)}");
-return;
